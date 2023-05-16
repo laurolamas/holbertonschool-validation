e5fda020df25463345d73fb62b38eaeb591695b9
Lauro <laurolamas@gmail.com>
2023-05-16 16:09:31 -0300

    aaaef

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -7,3 +7,7 @@ apt-get install -y wget make
 # Download and install Hugo
 wget https://github.com/gohugoio/hugo/releases/download/v0.55.0/hugo_0.55.0_Linux-64bit.deb
 dpkg -i hugo_0.55.0_Linux-64bit.deb
+
+#INSTALL golangcilint
+#export PATH="$PATH:/app/bin"
+#sudo curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s
