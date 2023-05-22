88a5993fa959f93d46ea82658f9c21e0531f9616
Lauro <laurolamas@gmail.com>
2023-05-21 21:27:48 -0300

    AAASA

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -5,9 +5,15 @@ apt-get update
 apt-get install -y wget make
 
 # Download and install Hugo
-wget https://github.com/gohugoio/hugo/releases/download/v0.55.0/hugo_0.55.0_Linux-64bit.deb
-dpkg -i hugo_0.55.0_Linux-64bit.deb
+wget https://github.com/gohugoio/hugo/releases/download/v0.85.0/hugo_0.85.0_Linux-64bit.deb
+sudo dpkg -i hugo_0.85.0_Linux-64bit.deb
 
-#INSTALL golangcilint
-#export PATH="$PATH:/app/bin"
-#sudo curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s
+## INSTALL golangcilint
+## export PATH="$PATH:/app/bin"
+## sudo curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s
+
+## Install Markdownlint
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
+
+export PATH="$PATH:/app/bin"
